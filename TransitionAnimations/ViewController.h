ece005dc82e32f23c7301fce66e663577d476b54
Logan Moseley <git@loganmoseley.com>
2015-03-18 09:50:46 -0400

    Initial Commit

---
+++ TransitionAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionAnimations
+//
+//  Created by Logan Moseley on 3/18/15.
+//  Copyright (c) 2015 CalamitySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

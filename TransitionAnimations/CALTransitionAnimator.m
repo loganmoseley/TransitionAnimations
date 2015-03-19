bf9dd69811757ab917cc77f9d508d53efa7da239
Logan Moseley <logan.moseley@nytimes.com>
2015-03-19 11:12:39 -0400

    Animate with snapshot views

--- TransitionAnimations/CALTransitionAnimator.m
+++ TransitionAnimations/CALTransitionAnimator.m
@@ -29,74 +29,96 @@
 }
 
 - (void)animatePresentation:(id <UIViewControllerContextTransitioning>)transitionContext {
-    UIViewController *source      = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    UIViewController *destination = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView           *container   = [transitionContext containerView];
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController *toViewController   = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIView           *containerView      = [transitionContext containerView];
     
     // iOS 8 doesn't use a rotation transform, but it does get the bounds of the destination wrong for landscape
     // (while container is right!).
-    destination.view.bounds = container.bounds;
+    toViewController.view.bounds = containerView.bounds;
     
-    [source.view layoutIfNeeded];
-    [destination.view layoutIfNeeded];
-    CGAffineTransform originalDestinationTransform = destination.view.transform;
-    CGAffineTransform originalSourceTransform = source.view.transform;
+    [fromViewController.view layoutIfNeeded];
+    [toViewController.view layoutIfNeeded];
+    CGAffineTransform originalFromTransform = fromViewController.view.transform;
+    CGAffineTransform originalToTransform = toViewController.view.transform;
     
-    CGRect fromRect = [(ViewController *)source transitionRectForTransitionContext:transitionContext];
-    CGRect toRect = [(CALImageViewController *)destination transitionRectForTransitionContext:transitionContext];
-    CGAffineTransform sourceToDestinationTransform = CGAffineTransformFromRectToRect(fromRect, toRect);
+    CGRect fromRect = [(ViewController *)fromViewController transitionRectForTransitionContext:transitionContext];
+    CGRect toRect = [(CALImageViewController *)toViewController transitionRectForTransitionContext:transitionContext];
+    CGAffineTransform fromToToTransform = CGAffineTransformFromRectToRect(fromRect, toRect);
     
-    destination.view.alpha = 0;
-    destination.view.transform = CGAffineTransformInvert(sourceToDestinationTransform);
-    [container addSubview:destination.view];
-    [source beginAppearanceTransition:NO animated:YES];
+    UIView *fromVCSnapshot = [[self class] snapshotViewFromView:fromViewController.view afterScreenUpdates:YES];
+    UIView *toVCSnapshot = [[self class] snapshotViewFromView:toViewController.view afterScreenUpdates:YES];
+    toVCSnapshot.alpha = 0;
+    toVCSnapshot.transform = CGAffineTransformInvert(fromToToTransform);
+    [containerView addSubview:fromVCSnapshot];
+    [containerView addSubview:toVCSnapshot];
+    
+    [fromViewController beginAppearanceTransition:NO animated:YES];
     
     [UIView animateWithDuration:[self transitionDuration:transitionContext]
                           delay:0
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
-                         destination.view.alpha = 1;
-                         destination.view.transform = originalDestinationTransform;
-                         source.view.transform = CGAffineTransformConcat(originalSourceTransform, sourceToDestinationTransform);
+                         fromVCSnapshot.transform = CGAffineTransformConcat(originalFromTransform, fromToToTransform);
+                         toVCSnapshot.alpha = 1;
+                         toVCSnapshot.transform = originalToTransform;
                      } completion:^(BOOL finished) {
-                         source.view.transform = originalSourceTransform;
-                         [source endAppearanceTransition];
+                         [fromVCSnapshot removeFromSuperview];
+                         [toVCSnapshot removeFromSuperview];
+                         [containerView addSubview:toViewController.view];
+                         [fromViewController endAppearanceTransition];
                          [transitionContext completeTransition:YES];
                      }];
 }
 
 // This method can only be a nop if the transition is interactive and not a percentDriven interactive transition.
 - (void)animateDismissal:(id <UIViewControllerContextTransitioning>)transitionContext {
-    UIViewController *source      = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    UIViewController *destination = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView           *container   = [transitionContext containerView];
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController *toViewController   = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIView           *containerView      = [transitionContext containerView];
     
-    [source.view layoutIfNeeded];
-    [destination.view layoutIfNeeded];
-    CGAffineTransform originalDestinationTransform = destination.view.transform;
-    CGAffineTransform originalSourceTransform = source.view.transform;
+    [fromViewController.view layoutIfNeeded];
+    [toViewController.view layoutIfNeeded];
+    CGAffineTransform originalToTransform = toViewController.view.transform;
+    CGAffineTransform originalFromTransform = fromViewController.view.transform;
     
-    CGRect fromRect = [(CALImageViewController *)source transitionRectForTransitionContext:transitionContext];
-    CGRect toRect = [(ViewController *)destination transitionRectForTransitionContext:transitionContext];
-    CGAffineTransform destinationToSourceTransform = CGAffineTransformFromRectToRect(toRect, fromRect);
+    CGRect fromRect = [(CALImageViewController *)fromViewController transitionRectForTransitionContext:transitionContext];
+    CGRect toRect = [(ViewController *)toViewController transitionRectForTransitionContext:transitionContext];
+    CGAffineTransform toToFromTransform = CGAffineTransformFromRectToRect(toRect, fromRect);
     
-    destination.view.alpha = 0;
-    destination.view.transform = CGAffineTransformConcat(originalDestinationTransform, destinationToSourceTransform);
-    [container addSubview:destination.view];
-    [destination beginAppearanceTransition:YES animated:YES];
+    UIView *fromVCSnapshot = [[self class] snapshotViewFromView:fromViewController.view afterScreenUpdates:YES];
+    UIView *toVCSnapshot = [[self class] snapshotViewFromView:toViewController.view afterScreenUpdates:YES];
+    toVCSnapshot.transform = CGAffineTransformConcat(originalToTransform, toToFromTransform);
+    [containerView addSubview:toVCSnapshot];
+    [containerView addSubview:fromVCSnapshot];
+    
+    [toViewController beginAppearanceTransition:YES animated:YES];
     
     [UIView animateWithDuration:[self transitionDuration:transitionContext]
                           delay:0
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
-                         destination.view.alpha = 1;
-                         destination.view.transform = CGAffineTransformIdentity;
-                         source.view.transform = CGAffineTransformConcat(originalSourceTransform, CGAffineTransformInvert(destinationToSourceTransform));
+                         fromVCSnapshot.alpha = 0;
+                         fromVCSnapshot.transform = CGAffineTransformConcat(originalFromTransform, CGAffineTransformInvert(toToFromTransform));
+                         toVCSnapshot.transform = originalToTransform;
                      } completion:^(BOOL finished) {
-                         [destination endAppearanceTransition];
-                         [source.view removeFromSuperview];
+                         [fromVCSnapshot removeFromSuperview];
+                         [toVCSnapshot removeFromSuperview];
+                         [containerView addSubview:toViewController.view];
+                         [fromViewController.view removeFromSuperview];
+                         [toViewController endAppearanceTransition];
                          [transitionContext completeTransition:YES];
                      }];
+}
+
++ (UIView *)snapshotViewFromView:(UIView *)inView afterScreenUpdates:(BOOL)afterScreenUpdates {
+    UIView *replicantView = [inView snapshotViewAfterScreenUpdates:afterScreenUpdates];
+    replicantView.alpha = inView.alpha;
+    replicantView.bounds = inView.bounds;
+    replicantView.center = inView.center;
+    replicantView.contentScaleFactor = inView.contentScaleFactor;
+    replicantView.transform = inView.transform;
+    return replicantView;
 }
 
 // http://stackoverflow.com/a/14764286/1621334 and Danny Zlobinsky
